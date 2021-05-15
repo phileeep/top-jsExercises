c51f463d5f1556dad65b995f6105a423c0fb1fde
Phil Smithies <philsmithies@gmail.com>
2021-05-15 18:06:39 +0200

    tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  const conversion = (number - 32) * .5556
+  return Math.round(conversion * 10) / 10
 }
 
-const ctof = function() {
-
+const ctof = function(number) {
+  const conversionFahreinheit = (number * 1.8) + 32
+  return Math.round(conversionFahreinheit * 10) / 10
 }
 
 module.exports = {
