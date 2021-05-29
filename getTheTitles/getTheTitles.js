44de14ef5ddd02203e416701ce06a77cd622b742
“Phil <philsmithies@gmail.com>
2021-05-29 11:13:53 +0200

    get the titles of books

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  const getTitles = []
+  books.forEach(book => getTitles.push(book.title))
+  return getTitles
 }
 
 module.exports = getTheTitles;
